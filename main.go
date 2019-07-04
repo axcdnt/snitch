b0989c23237879200e1a6737d34669392a5ca2b1
Ricardo Maraschini <rmarasch@redhat.com>
2019-07-04 18:12:05 +0200

    Refactoring for easier code reading.
    This patch refactors some of the functions to make them easier to
    read and understand.

--- main.go
+++ main.go
@@ -75,7 +75,6 @@ func walk(rootPath *string) FileInfo {
 	if err := filepath.Walk(*rootPath, visit(watchedFiles)); err != nil {
 		log.Fatal("walk:", err)
 	}
-
 	return watchedFiles
 }
 
@@ -94,34 +93,38 @@ func visit(watchedFiles FileInfo) filepath.WalkFunc {
 }
 
 func runOrSkipTest(filePath string) {
-	if isRegularFile(filePath) {
-		// run the respective test for go files
-		testFilePath := findTestFilePath(filePath)
-		if testFilePath != "" {
-			test(testFilePath)
-		}
-	} else {
-		// run tests directly
-		test(filePath)
+	testFilePath := findTestFilePath(filePath)
+	if testFilePath == "" {
+		// there is no test file for the modified path, ignore.
+		return
 	}
+	test(testFilePath)
 }
 
 func findTestFilePath(filePath string) string {
-	fileName := path.Base(filePath)
-	if isRegularFile(fileName) {
-		path := path.Dir(filePath)
-		extensionLessFileName := strings.Split(fileName, ".")[0]
-		testFilePath := filepath.Join(path, extensionLessFileName+"_test.go")
-		if _, ok := watchedFiles[testFilePath]; ok {
-			return testFilePath
-		}
+	if isTestFile(filePath) {
+		// change happened on a test file, just return its path.
+		return filePath
 	}
 
-	return ""
+	// changed file is not a test file, we need to change its path to
+	// contain an _test.go suffix then return the full path. if test
+	// file for the changed path does not exist, returns empty string
+	// instead.
+	fileName := path.Base(filePath)
+	path := path.Dir(filePath)
+	extensionLessFileName := strings.Split(fileName, ".")[0]
+	testFilePath := filepath.Join(path, extensionLessFileName+"_test.go")
+	if _, ok := watchedFiles[testFilePath]; !ok {
+		// no test file exists, return empty.
+		return ""
+	}
+
+	return testFilePath
 }
 
-func isRegularFile(fileName string) bool {
-	return !strings.HasSuffix(fileName, "_test.go")
+func isTestFile(fileName string) bool {
+	return strings.HasSuffix(fileName, "_test.go")
 }
 
 func test(filePath string) {
